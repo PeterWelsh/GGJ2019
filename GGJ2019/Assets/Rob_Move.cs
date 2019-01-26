16991962509e537875c45f6817c780248bee9633
ROBERT GEMMILL <1600750@uad.ac.uk>
2019-01-26 00:08:34 +0000

    testing

---
+++ GGJ2019/Assets/Rob_Move.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rob_Move : MonoBehaviour {
+    float speed = 3.0f;
+   public int type;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(speed * Time.deltaTime, 0.0f, 0.0f);
+	}
+
+  
+}
